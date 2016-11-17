ff7e42ed68d2f6a9a32f5e6c8946896aa0ad1ac5
Jancleidsson Soares <jancleidsson@gmail.com>
2016-11-17 13:15:44 -0300

    Changes: Added routers for contact list and new contact

--- angular-2-beta-boilerplate-master/dev/app.component.ts
+++ angular-2-beta-boilerplate-master/dev/app.component.ts
@@ -2,16 +2,40 @@ import {Component} from 'angular2/core';
 import {MyComponentComponent} from './my-component.component';
 import {MyInformationComponent} from './my-information.component';
 import {ContactsComponent} from './contact/contacts.component';
+import {NewContactComponent} from './contact/new-contact.component';
+import {ROUTER_DIRECTIVES} from 'angular2/router'
+import {RouteConfig} from 'angular2/router'
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>My app</h1>
         <!-- <my-component></my-component> -->
         <!-- <my-information></my-information> -->
-        <contacts-component></contacts-component>
-    `, directives: [ContactsComponent]
+        <header>
+            <nav>
+                <a [routerLink]="['Contacts']">Contacts</a>
+                <a [routerLink]="['NewContact']">New Contact</a>
+            </nav>
+        </header>
+        <div class = "main">
+            <router-outlet></router-outlet>
+        </div>
+    `, directives: [ContactsComponent, NewContactComponent, ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+    {
+        path: '/contacts',
+        name: 'Contacts',
+        component: ContactsComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/newcontact',
+        name: 'NewContact',
+        component: NewContactComponent
+    }
+])
 export class AppComponent {
 
 }
---
+++ angular-2-beta-boilerplate-master/dev/contact/new-contact.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    template: `
+        Creating a new contact
+    `
+})
+
+export class NewContactComponent {
+
+}
